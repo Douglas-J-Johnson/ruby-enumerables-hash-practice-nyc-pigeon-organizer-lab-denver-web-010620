92fecb5d6d919e66d54f5a7103282cdba1fad184
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-31 13:39:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,18 +18,17 @@ def condense_pairs(hash_array)
   value = ""
 
   hash_array.each do |hash|
-    puts hash
     hash.each do |k, v|
       name = k
       value = v
     end
 
-    #if find_name_in_hash_array(condensed_hash_array,name)
-    #  condensed_index = find_name_in_hash_array(condensed_hash_array,name)
-    #  condensed_hash_array[condensed_index][name].push(value)
-    #else
+    if find_name_in_hash_array(condensed_hash_array,name)
+      condensed_index = find_name_in_hash_array(condensed_hash_array,name)
+      condensed_hash_array[condensed_index][name].push(value)
+    else
       condensed_hash_array.push(hash)
-    #end
+    end
   end
 
   pp condensed_hash_array
