8de053d8f29a227f88335f3bea3aa56e18e7bf4e
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-31 14:31:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -31,6 +31,8 @@ def condense_pairs(hash_array)
       value = v
     end
 
+    puts name
+
     if find_name_in_hash_array(condensed_hash_array,name)
       condensed_index = find_name_in_hash_array(condensed_hash_array,name)
       condensed_hash_array[condensed_index][name].push(value)
